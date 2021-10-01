f8d317d9c6d55a85e4a4afd02c560bc3331be6bf
Wootten <harrisonwootten@hotmail.com>
2021-10-01 09:19:44 +0100

    Added springboot functionality (get method)

--- src/main/java/zooKeeper/Main.java
+++ src/main/java/zooKeeper/Main.java
@@ -1,10 +1,24 @@
 package zooKeeper;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
 public class Main {
 
     public static void main(String[] args) {
-        /**
-         * Currently empty
-         */
+        SpringApplication.run(Main.class, args);
+    }
+    @CrossOrigin("http://localhost:3000")
+    @GetMapping("/customRoute")
+    public String myZoo() {
+        Zoo zooAnimals = new Zoo();
+        return zooAnimals.toString();
     }
 }
---
+++ src/main/java/zooKeeper/Zoo.java
@@ -0,0 +1,26 @@
+package zooKeeper;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+    private List<Animal> zooAnimals = new ArrayList();
+
+    public Zoo() {
+        //this.zooAnimals.add(new Cat(20, "Tiger", 5, 40));
+        this.zooAnimals.add(new CatFish(50,70));
+        this.zooAnimals.add(new Bat(true));
+        this.zooAnimals.add(new Pigeon(70));
+    }
+
+    public List<Animal> getZooAnimals() {
+        return this.zooAnimals;
+    }
+
+    public String toString() {
+        String json = new Gson().toJson(zooAnimals);
+        return json;
+    }
+}
